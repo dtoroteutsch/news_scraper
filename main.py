b391a184e57200cd365bd89c19a599e02673e97b
dtoroteutsch <toro.teutsch.d@gmail.com>
2020-09-02 21:20:50 -0400

    Getting and saving title and content news articles

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import argparse
 import logging
+import re
+import datetime
+import csv
+
+from requests.exceptions import HTTPError
+from urllib3.exceptions import MaxRetryError
 
 import news_page_objects as news
 
@@ -8,13 +14,59 @@ logging.basicConfig(level = logging.INFO)
 from common import config
 
 logger = logging.getLogger(__name__)
+is_well_formed_link = re.compile(r'^https?://.+/.+$') #https://example.com/hello
+is_root_path = re.compile(r'^/.+$') # some-text
 
 def _news_scraper(news_site_uid):
     host = config()['news_sites'][news_site_uid]['url']
     logging.info(f'Beginning scraper for {host}')
     homepage = news.HomePage(news_site_uid, host)
+
+    articles = list()
     for link in homepage.article_links:
-        print(link)
+        article = _fetch_article(news_site_uid, host, link)
+        if article:
+            logger.info('Article fetched!!')
+            articles.append(article)
+
+    _save_articles(news_site_uid, articles)
+
+def _save_articles(news_site_uid, articles):
+    now = datetime.datetime.now().strftime('%Y_%m_%d')
+    out_file_name = f'{news_site_uid}_{now}_articles.csv'
+    csv_headers = list(filter(lambda property: not property.startswith('_'), dir(articles[0])))
+    
+    with open(out_file_name, mode = 'w+') as file:
+        writer = csv.writer(file)
+        writer.writerow(csv_headers)
+
+        for article in articles:
+            row = [str(getattr(article, prop)) for prop in csv_headers]
+            writer.writerow(row)
+
+
+def _fetch_article(news_site_uid, host, link):
+    logger.info(f'Fetching article at {link}')
+    article = None
+    try:
+        article = news.ArticlePage(news_site_uid, _build_link(host, link))
+    except (HTTPError, MaxRetryError) as e:
+        logger.warning('Error while fetching the article', exc_info = False)
+
+    if article and not article.body:
+        logger.warning('Invalid article, There is no body')
+        return None
+
+    return article
+
+
+def _build_link(host, link):
+    if is_well_formed_link.match(link):
+        return link
+    elif is_root_path.match(link):
+        return f'{host}{link}'
+    else:
+        return '{host}/{uri}'.format(host = host, uri = link)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
